7cfcbf6f65a1c3e55331e063267fecf23cc5a17b
Ritik Wakchaure <rutikwakchaure805@gmail.com>
2026-08-03 22:17:24 +0530

    Solve PizzaOrders using do-while loop

--- src/main/java/com/chaicode/dowhileloop/PizzaOrders.java
+++ src/main/java/com/chaicode/dowhileloop/PizzaOrders.java
@@ -1,27 +1,32 @@
 package com.chaicode.dowhileloop;
 
-/**
- * SliceHouse Pizza Orders
- *
- * <p>Count how many pizzas were ordered from a list of order codes until "DONE" appears.
- * Uses a do-while loop to process at least one entry when the array is non-empty.
- *
- * <p>Rules:
- * <ul>
- *   <li>Count non-null, non-empty, non-"DONE" entries until "DONE" is seen</li>
- *   <li>"DONE" itself is not counted</li>
- *   <li>If orders is null or empty, return 0</li>
- * </ul>
- *
- * @param orders array of order codes, ending with "DONE"
- * @return number of pizzas ordered
- */
 public final class PizzaOrders {
 
     private PizzaOrders() {}
 
     public static int countOrders(String[] orders) {
-        // Your code here
-        return 0;
+
+        if (orders == null || orders.length == 0) {
+            return 0;
+        }
+
+        int count = 0;
+        int i = 0;
+
+        do {
+
+            if ("DONE".equals(orders[i])) {
+                break;
+            }
+
+            if (orders[i] != null && !orders[i].isEmpty()) {
+                count++;
+            }
+
+            i++;
+
+        } while (i < orders.length);
+
+        return count;
     }
 }
